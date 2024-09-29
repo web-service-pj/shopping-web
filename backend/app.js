11ec582f9888281bcb95c6250da82d8a64b99bf8
NOHYEONGJUN <n7804@naver.com>
2024-09-29 21:11:29 +0900

    fix : CI/CD Front & Back 분리
    Jenkinsfile-backend
    Jenkinsfile-frontend

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // react 포트 맞추기
 const cors = require('cors');
 app.use(cors({
-    origin: 'http://localhost:3000', // React 앱의 주소
+    origin: 'http://113.198.66.75:18076', // React 앱의 주소
     credentials: true,
   }));
 
