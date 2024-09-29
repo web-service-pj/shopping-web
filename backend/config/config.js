ea5fe6264a31005010ab047dd00854e3afbeb6b9
NOHYEONGJUN <n7804@naver.com>
2024-09-29 22:39:15 +0900

    fix : backend config.js 수정

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,27 +2,27 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: "user",
+    password: "root",
+    database: "shop_db",
+    host: "113.198.66.75",
+    port: "13047",
     dialect: "mysql"
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: "user",
+    password: "root",
+    database: "shop_db",
+    host: "113.198.66.75",
+    port: "13047",
     dialect: "mysql"
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: "user",
+    password: "root",
+    database: "shop_db",
+    host: "113.198.66.75",
+    port: "13047",
     dialect: "mysql",
     logging: false,
     pool: {
